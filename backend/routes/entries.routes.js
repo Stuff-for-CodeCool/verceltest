bcaa742685768d29d1c3c3c0e0a13d0e5c40cdcc
alexsincai <alex.sincai@yahoo.co.uk>
2023-03-20 11:05:54 +0200

    fix ?

--- backend/routes/entries.routes.js
+++ backend/routes/entries.routes.js
@@ -4,14 +4,13 @@ const { Router } = require("express");
 const { connect } = require("mongoose");
 const Entries = require("../database/entry.model");
 
-const connection = connect(process.env.MONGO_URL);
-
 module.exports = Router()
     .use("/", (req, res, next) => {
         res.setHeader("Cache-Control", "s-max-age=1, stale-while-revalidate");
         next();
     })
     .get("/populate", async (req, res) => {
+        const connection = await connect(process.env.MONGO_URL);
         await Entries.deleteMany({});
 
         const dataToBeAdded = Array(20)
@@ -19,28 +18,41 @@ module.exports = Router()
             .map((_, i) => ({ text: `This is the text for entry ${i + 1}` }));
 
         await Entries.insertMany(dataToBeAdded);
+        await connection.disconnect();
         res.json({ message: "ok" });
     })
     .get("/", async (req, res) => {
-        res.json(await Entries.find({}).sort({ updatedAt: -1 }));
+        const connection = await connect(process.env.MONGO_URL);
+        const entries = await Entries.find({}).sort({ updatedAt: -1 });
+        await connection.disconnect();
+        res.json(entries);
     })
     .post("/", async (req, res) => {
+        const connection = await connect(process.env.MONGO_URL);
         res.json(await Entries.create({ text: req.body.text }));
+        res.json(await Entries.create({ text: req.body.text }));
+        await connection.disconnect();
     })
     .get("/:id", async (req, res) => {
-        res.json(await Entries.findById(req.params.id));
+        const connection = await connect(process.env.MONGO_URL);
+        const entries = await Entries.findById(req.params.id);
+        await connection.disconnect();
+        res.json(entries);
     })
     .put("/:id", async (req, res) => {
-        res.json(
-            await Entries.findOneAndUpdate(
-                { _id: req.params.id },
-                { text: req.body.text, updatedAt: Date.now() }
-            )
+        const connection = await connect(process.env.MONGO_URL);
+        const updated = await Entries.findOneAndUpdate(
+            { _id: req.params.id },
+            { text: req.body.text, updatedAt: Date.now() }
         );
+        await connection.disconnect();
+        res.json(updated);
     })
     .delete("/:id", async (req, res) => {
+        const connection = await connect(process.env.MONGO_URL);
         const { id } = req.params;
         const deleted = await Entries.deleteOne({ _id: id });
+        await connection.disconnect();
 
         if (deleted.deletedCount > 0) {
             res.json({ message: `Deleted entry ${id}` });
